597c11b8b73c0f6d17de66090e21607667d249c0
mayar-kabaja <kabajamayar@gmail.com>
2022-06-23 17:20:16 +0300

    fix index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
+import Card from './Card/Card';
+import Filters from './Filters/Filters';
 import NavBar from './NavBar/NavBar';
 import { Logo } from './UI';
 
-export { Logo, NavBar };
+export {
+  Logo, NavBar, Filters, Card,
+};
