2cf62d27e8e1932814db657e1073d63234a876c5
mayar-kabaja <kabajamayar@gmail.com>
2022-06-23 17:20:37 +0300

    fix Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import { Card as CardAnt, Rate, Typography, Space } from 'antd';
+import PropTypes from 'prop-types';
+import {
+  Card as CardAnt, Rate, Typography, Space,
+} from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 
 const { Meta } = CardAnt;
 const { Title } = Typography;
 
-function Card({ title, rating, description, price, image, id, loading }) {
+function Card({
+  title, rating, price, image, id, loading,
+}) {
   return (
     <CardAnt
       hoverable
       loading={loading}
       style={{
         width: 240,
+        marginBottom: 30,
       }}
-      cover={<img alt="example" src={image} style={{ width: '95%' }} />}
+      cover={<img alt="example" src={image} style={{ width: '95%', height: '250px' }} />}
     >
       <Title level={5}>
         {price}
         <span>$</span>
       </Title>
-      <Meta title={title} description={description} key={id} />
+      <Meta title={title} key={id} />
       <Space align="center" size="large">
         <Rate allowHalf defaultValue={rating} />
         <ShoppingCartOutlined style={{ fontSize: '25px', cursor: 'pointer', display: 'block' }} />
@@ -28,3 +34,11 @@ function Card({ title, rating, description, price, image, id, loading }) {
   );
 }
 export default Card;
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
